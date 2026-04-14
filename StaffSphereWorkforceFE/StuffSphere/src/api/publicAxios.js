dab131e8ee2e784536e6e32679f274f2bb05c5f3
bantupallipavithra <bantupalli302@gmail.com>
2026-04-14 16:14:29 +0530

    changes

--- StaffSphereWorkforceFE/StuffSphere/src/api/publicAxios.js
+++ StaffSphereWorkforceFE/StuffSphere/src/api/publicAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const publicAxios = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://staffsphere-i8p3.onrender.com/api",
 });
 
 export default publicAxios;
